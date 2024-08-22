c2b2d8e5d0c8d3d4ab4ec9cf13afd6654025191f
Vaibhav Singh <58362045+vaibhav29-indi@users.noreply.github.com>
2024-08-22 13:58:27 +0530

    Update streamlit_app.py
    Added requests to fruityverse API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -44,4 +45,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
